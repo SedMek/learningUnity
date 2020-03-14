9f38b8f6e12ed3c8dc4f672a6f1ba85c127acd52
Seddik <s.mekki@kayrros.com>
2020-03-14 14:52:12 +0100

    initial commit

---
+++ Assets/playerCollision.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class playerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+
+    // Start is called before the first frame update
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag != "ground")
+        {
+            movement.enabled = false;
+        }
+    }
+}
---
+++ Assets/playerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+
+    public float forwardStep = 50f;
+    public float sideStep = 20f;
+    public Rigidbody rb;
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, forwardStep * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -forwardStep * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("space"))
+        {
+            rb.AddForce(0, sideStep * Time.fixedDeltaTime, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sideStep * Time.fixedDeltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sideStep * Time.fixedDeltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+    }
+}
